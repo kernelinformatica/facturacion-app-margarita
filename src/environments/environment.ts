2b6447b191363b522b347ade503ec4345405da48
Dario Quiroga <dario@kernelinformatica.com.ar>
2023-03-06 11:29:40 -0300

    Actualización 2023-03-06

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,21 @@
 export const environment = {
     production: false,
     facturacionRest: {
-       urlBase: 'http://192.168.140.119:8080/facturacionrest-margarita-v13/ws', 
-       
-       //urlBase: 'http://localhost:8080/facturacionrest/ws', 
-       urlFactElectronica: 'http://localhost:8080/xxxxxxx/ws',        
+       urlBase: 'http://10.0.0.212:8080/facturacionrest-margarita-v14/ws',
+
+       //urlBase: 'http://localhost:8080/facturacionrest/ws',
+       urlFactElectronica: 'http://localhost:8080/xxxxxxx/ws',
         timeoutDefault: 60000
     },
-    localStorage: { 
+    localStorage: {
         acceso: 'accesoActivo',
         menu: 'menuActivo',
         perfil: 'perfilActivo',
         usuario: 'usuarioActivo',
         cuenta: 'cuenta'
     },
-    versionLocal: { 
-        numero: '1.1.1', 
+    versionLocal: {
+        numero: '1.1.1',
         descripcion: 'Produccion'
     }
 };
