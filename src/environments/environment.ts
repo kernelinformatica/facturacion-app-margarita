174b6ddc78c8451ca25f2e01a39446ef27d3e265
Dario Quiroga <dario@kernelinformatica.com.ar>
2023-03-16 10:47:29 -0300

    Se modifico consulta de comprobantes, se quito el paginado por pedido de Marisa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     facturacionRest: {
        urlBase: 'http://10.0.0.212:8080/facturacionrest-margarita-v14/ws',
-
+      // urlBase: 'http://192.168.140.119:8080/facturacionrest-margarita-v14/ws',
        //urlBase: 'http://localhost:8080/facturacionrest/ws',
        urlFactElectronica: 'http://localhost:8080/xxxxxxx/ws',
         timeoutDefault: 60000
