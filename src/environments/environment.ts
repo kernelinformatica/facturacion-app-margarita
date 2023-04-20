90a19710d18fe02ec682b7ae14c00692f2305d7a
Dario Quiroga <dario@kernelinformatica.com.ar>
2023-04-20 16:20:54 -0300

    lista de precios, modificaciones

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     versionSistema: "8.9",
     facturacionRest: {
-        urlBase: 'http://10.0.0.212:8080/facturacionrest-margarita-v18/ws',
-        // urlBase: 'http://192.168.140.119:8080/facturacionrest-margarita-v14/ws',
+       // urlBase: 'http://10.0.0.212:8080/facturacionrest-margarita-v18/ws',
+         urlBase: 'http://192.168.140.119:8080/facturacionrest-margarita-v18/ws',
         //urlBase: 'http://localhost:8080/facturacionrest/ws',
         urlFactElectronica: 'http://localhost:8080/xxxxxxx/ws',
         timeoutDefault: 60000
